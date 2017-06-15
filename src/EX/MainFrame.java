52d2a6ea0e71922e0a57137b763610ce3b483a0c
a298582548 <105021010@live.asia.edu.tw>
2017-06-16 01:23:33 +0800

    001

--- src/EX/MainFrame.java
+++ src/EX/MainFrame.java
@@ -12,29 +12,29 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainFrame extends Frame  implements ActionListener{
-	private Button b1=new Button ("中文");
-	private Button b2=new Button ("英文");
+	private JButton b1=new JButton ("中文");
+	private JButton b2=new JButton ("英文");
 
+	private JButton b3=new JButton ("開始");
+	private JButton b4=new JButton ("重新開始");
+	private JButton b5=new JButton ("判斷");
 
-	private Button b3=new Button ("開始");
-	private Button b4=new Button ("重新開始");
-	private Button b5=new Button ("判斷");
-
-	private Label lab1=new Label  ("中英文打字系統");
-	private Label lab2=new Label  ("60");
-	private Label lab3=new Label  ("算算你打了0個字");
-	private Label lab4=new Label  ("錯了幾個字");
+	private JLabel lab1=new JLabel  ("中英文打字系統",SwingConstants.CENTER);
+	private JLabel lab2=new JLabel  ("60",SwingConstants.CENTER);
+	private JLabel lab3=new JLabel  ("你打了0個字",SwingConstants.CENTER);
+	private JLabel lab4=new JLabel  ("錯了幾個字",SwingConstants.CENTER);
 	
 
 	private Panel pn1=new Panel();
 	private Panel pn2=new Panel();
-  
+    private Panel pn3=new Panel();
      
-    private Label ta=new Label();	
-    private TextField tf=new TextField();
+    private JLabel ta=new JLabel();	
+    private JTextField tf=new JTextField();
     private int tfCount =0;
 	private Timer tmr;
-    
+    private Font ss = new Font("標楷體", Font.BOLD, 20);
+   
     public MainFrame(){
 		 initComp();
 		 Timer tmr; 
@@ -48,7 +48,7 @@ public class MainFrame extends Frame  implements ActionListener{
     	// 宣告JComboBox
     	JComboBox comebox = new JComboBox();
     	// 增加內容到comebox
-    			
+    	this.setTitle("中英打練習系統");	
 		 this.addWindowListener(new WindowAdapter(){
 			 public void windowClosing( WindowEvent e){
 				 System.exit(0);
@@ -57,20 +57,15 @@ public class MainFrame extends Frame  implements ActionListener{
 		 
          this.setBounds(650, 250, 915, 800);
 		 this.setLayout(new BorderLayout(5,5));
-		 ta.setFont(new Font("標楷體", Font.BOLD, 15));
-		 ta.setBackground(Color.CYAN);
-		 lab1.setAlignment(Label.CENTER);
-		 lab2.setAlignment(Label.CENTER);
-		 lab3.setAlignment(Label.CENTER);
-		 lab4.setAlignment(Label.CENTER);
-		 
-		 lab1.setBackground(Color.PINK);
-         pn1.setLayout(new GridLayout(8,1,5,5));
-         pn2.setLayout(new GridLayout(2,1,3,3));
+		 lab1.setFont(new Font("標楷體", Font.BOLD, 20));
+		 ta.setFont(ss);lab3.setFont(ss); lab4.setFont(ss);lab2.setFont(ss);
 
+		 lab1.setBackground(Color.PINK);
+         pn1.setLayout(new GridLayout(1,1,1,-15));
+         pn2.setLayout(new GridLayout(2,1,1,0));
+         pn3.setLayout(new GridLayout(2,1,1,0));
          pn1.add(lab1);
-         pn1.add(lab2);
-         
+         pn1.add(lab2);     
          pn1.add(comebox);
         comebox.addItem("中文");
         comebox.addItem("英文");
@@ -78,8 +73,8 @@ public class MainFrame extends Frame  implements ActionListener{
 
          pn1.add(b3);
          pn1.add(b4);
-         pn1.add(lab4);
-         pn1.add(lab3);
+         pn3.add(lab3);
+         pn3.add(lab4);
          pn1.add(b5);
 
        
@@ -88,8 +83,8 @@ public class MainFrame extends Frame  implements ActionListener{
          pn2.add(tf);
          this.add(pn1,BorderLayout.NORTH);         
          this.add(pn2,BorderLayout.CENTER);
-      
-         
+         this.add(pn3,BorderLayout.SOUTH); 
+         tf.setEnabled(false);
          
 
 				
@@ -132,21 +127,29 @@ public class MainFrame extends Frame  implements ActionListener{
 					//} catch (Exception e1) {} 	 									
 					//lab2.setText("倒數"+i+"秒"); 
 					//} 
+				tf.setEnabled(true);
+				b3.setEnabled(false);
 				
 				if(comebox.getSelectedItem().equals("中文")){
 					ta.setText(fun1());
 				}else{
 					ta.setText(fun2());
-				}
-				
-			}
-
-			
-				
+				}			
+			}				
 		 });
-
+		 b4.addActionListener(new ActionListener(){		 
+				public void actionPerformed(ActionEvent ae) {
+					b3.setEnabled(true);
+					lab4.setText("錯了0幾個字");
+					lab3.setText("錯了幾個字");
+					tf.setEnabled(true);
+					ta.setText("");
+					tf.setText("");
+				}
+		 });
 		 tf.addKeyListener(new KeyAdapter(){
 			 public void keyTyped(KeyEvent ke){
+				
 			 if(ke.getKeyChar()==8 && tfCount>0){
 				 tfCount--;
 				 }else if(ke.getKeyChar()!=8){
@@ -162,6 +165,7 @@ public class MainFrame extends Frame  implements ActionListener{
     
     b5.addActionListener(new ActionListener(){
 		public void actionPerformed(ActionEvent aw){
+		tf.setEnabled(false);
 		int IT = 0;
 		char[]v1 = (ta.getText()).toCharArray();
 		char[]v2 = (tf.getText()).toCharArray();
@@ -178,7 +182,12 @@ public class MainFrame extends Frame  implements ActionListener{
 
 
     
-		 private String fun1(){
+		 private void add(ImageIcon imageIcon) {
+		// TODO Auto-generated method stub
+		
+	}
+
+		private String fun1(){
      		Random rnd = new Random();
      		String s = null ;
      		int v1 = rnd.nextInt(4)+1;
@@ -205,16 +214,16 @@ public class MainFrame extends Frame  implements ActionListener{
 				int v2 = rnd.nextInt(4)+1;
 				switch(v2){
 				case 1:
-					s1 = "For student satoneelementaryschoolgoingtoclassmeanstakingbookspencilsandpassportsRtyuugesl";
+					s1 = "His breakthrough major title at the 2016 US Open paved the way for a streak which has yielded five more wins,including three this season,and catapulted him to world No.1";
 					break;
 				case 2:
-					s1 = "JimmyKimmelandTrevorNoahmockedPresidentTrumpsdecisiontowithdrawfromtheParisClimateAccord";
+					s1 = "The medieval era took the model of masculinity back towards basic,chivalric,Christian values,before the likes of Byron and Brummell shattered these ideas with the rise of the dandy";
 					break;
 				case 3:
-					s1 = "TheadvertisementisamusicvideothatfeaturesarecreatedterroristattackandlyricsaskingRytIEgd";
+					s1 = "There cannot be one singular kind of Nigerian man or woman,there has to be room for other definitions that dont necessarily fit that opinion";
 					break;
 				case 4:
-					s1 = "WhenyouthinkofahackeryouprobablythinkaboutbasementdwellersinhoodiestypingDtehyjkQluljTuy";
+					s1 = "His amazing life has now become the subject of an international book,published by Bloomsbury and endorsed by King Abdullah II of Jordan and British adventurer Ranulph Fiennes";
 					break;
 				}
 				return s1;
